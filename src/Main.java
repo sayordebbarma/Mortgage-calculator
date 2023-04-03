f0b5b54682e4c664338c67233dbbe277c208d142
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-04-04 00:14:32 +0530

    added range functionality

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,40 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("principal loan amount: ");
-        int principal = scanner.nextInt();
+        int principal;
+        while (true) {
+            System.out.print("principal loan amount: ");
+            principal = scanner.nextInt();
+            if (principal >= 1_000 && principal <= 1_000_000)
+                break;
+            System.out.println("Please enter value between 1000 and 1000000");
+        }
 
-        System.out.print("Annual interest rate ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / percent / month_in_year;
-        System.out.println(monthlyInterest);
+        float monthlyInterest;
+        while (true) {
+            System.out.print("Annual interest rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30) {
+                monthlyInterest = annualInterest / percent / month_in_year;
+                break;
+            }
+            System.out.println("Please enter value between 1 and 30");
+        }
 
-        System.out.print("Period (years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * month_in_year;
+        int numberOfPayments;
+        while (true) {
+            System.out.print("Period (years): ");
+            byte years = scanner.nextByte();
+            if (years >=1 && years <= 30) {
+                numberOfPayments = years * month_in_year;
+                break;
+            }
+            System.out.println("Please enter value between 1 and 30");
+        }
 
-        double mortgageCal = principal * ((monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments)) +1);
+        double mortgageCal = principal * ((monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments)) + 1);
 
-
-        System.out.println("Therefore the mortgage will be " + mortgageCal);
+        System.out.println("Therefore the mortgage will be " + Math.round(mortgageCal));
 
     }
 }
